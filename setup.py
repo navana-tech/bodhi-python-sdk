0f86907cbe9a3bc679fc5c658a4ed06d96173686
Parth <parth@navanatech.in>
2025-06-05 00:45:01 +0530

    fix(setup): update version number to 1.0.0.post1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bodhi-sdk",
-    version="1.0.0",
+    version="1.0.0.post1",
     packages=["bodhi", "bodhi.utils"],
     install_requires=[
         "requests",
